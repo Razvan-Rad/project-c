1a4e042181202c67a0e310f3ebbf1bf2e67bce6f
Rad Razvan <rad.razvan.daniel@gmail.com>
2025-05-22 17:36:59 +0200

    update shadcn config

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 <h4>Update config</h4>
 
 ```
-docker-compose build
+docker-compose build devenv devweb
 ```
 <h4>Run container</h4>
 
 ```
 docker-compose up devenv
+OR
+docker-compose up devweb
 ```
 
 <h4>attach</h4>
--- project-outsourcing/src/App.tsx
+++ project-outsourcing/src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button } from "@/components/ui/button"
+ 
 function App() {
-
   return (
-    <>
-      <h1>yzxxx</h1>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   )
 }
-
+ 
 export default App
